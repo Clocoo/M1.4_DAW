45404451863526358d019aaf8a41d8104f17ea26
Osmar Torres <78390876+Clocoo@users.noreply.github.com>
2023-08-30 23:53:28 -0700

    versión 3 servicioDatos.js
    Utilizando Promises y .then y .catch

--- servicioDatos.js
+++ servicioDatos.js
@@ -1,9 +1,19 @@
-let finDeTransmisionDeDatos = function (id,callback){
-    console.log('Transferencia',id,'terminada');
-    callback();
-}
-let obtenDatosDeInternet = function (id,duracion,callback){
-    console.log('Proceso',id,'obteniendo datos de Internet');
-    setTimeout(finDeTransmisionDeDatos,duracion,id,callback);
-}
+const finDeTransmisionDeDatos = function(id) {
+    return new Promise(function(resolve) {
+        console.log('Transferencia', id, 'terminada');
+        resolve(id);
+    });
+};
+
+const obtenDatosDeInternet = function(id, duracion) {
+    console.log('Proceso', id, 'obteniendo datos de Internet');
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            finDeTransmisionDeDatos(id)
+                .then(resolve)
+                .catch(reject);
+        }, duracion);
+    });
+};
+
 module.exports.getDatos = obtenDatosDeInternet;
