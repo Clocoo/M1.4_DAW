c613c2464b1e87be45bc2cfd98264bb3dcae68d2
Osmar Torres <78390876+Clocoo@users.noreply.github.com>
2023-08-30 23:58:03 -0700

    Versión 4 - clienteDatos.js
    Utilizando Promises con async/await.

--- clienteDatos.js
+++ clienteDatos.js
@@ -1,28 +1,19 @@
 const servicio = require("./servicioDatos");
 
-const obtenerDatos = function(id, duracion) {
-    return new Promise(function(resolve, reject) {
-        servicio.getDatos(id, duracion)
-            .then(function() {
-                resolve(id);
-            })
-            .catch(reject);
-    });
-};
+async function obtenerDatos(id, duracion) {
+    await servicio.getDatos(id, duracion);
+}
 
-obtenerDatos(1, 3000)
-    .then(function() {
-        return obtenerDatos(2, 500);
-    })
-    .then(function() {
-        return obtenerDatos(3, 4000);
-    })
-    .then(function() {
-        return obtenerDatos(4, 700);
-    })
-    .then(function() {
-        return obtenerDatos(5, 3500);
-    })
-    .catch(function(error) {
+async function realizarObtencionDatos() {
+    try {
+        await obtenerDatos(1, 3000);
+        await obtenerDatos(2, 500);
+        await obtenerDatos(3, 4000);
+        await obtenerDatos(4, 700);
+        await obtenerDatos(5, 3500);
+    } catch (error) {
         console.error('Error:', error);
-    });
+    }
+}
+
+realizarObtencionDatos();
